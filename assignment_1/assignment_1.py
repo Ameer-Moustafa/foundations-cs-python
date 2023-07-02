d5de617302e2502f1cc80028d848fca14d5cbe37
ForbiddenShadow <forbiddenshadow@protonmail.com>
2023-07-02 21:46:10 +0300

    added code documentation in form of comments

--- assignment_1/assignment_1.py
+++ assignment_1/assignment_1.py
@@ -36,20 +36,22 @@ print(678%3*(8-(9/4)))
 # Question 2 #
 ##############
 
+# Below is a function that takes user input, stores it in variables and prints a new profile for the user.
+
 def profileGenerator():
-    ID = int(input('Enter your ID:'))
-    NAME = input('Enter your Name:').replace("  ", " ")
-    DOB = input('Enter your date of birth (DD-MM-YYYY or DD/MM/YYYY format):')
-    while(len(DOB) < 10 or len(DOB) > 10):
+    ID = int(input('Enter your ID:')) # Saves the user's ID into a variable
+
+    NAME = input('Enter your Name:').replace("  ", " ") # Saves the user's name into a variable
+
+    DOB = input('Enter your date of birth (DD-MM-YYYY or DD/MM/YYYY format):') # Saves the user's date of birth into a variable
+
+    while(len(DOB) < 10 or len(DOB) > 10): # A loop that asks the user for a new input if they don't input their date of birth correctly
         DOB = input('Please input your date of birth with the proper format (ex: 03/05/1995 or 03-05-1995): ')
 
-    ADDRESS = input('Enter your address:').replace("  ", " ")
+    ADDRESS = input('Enter your address:').replace("  ", " ") # Saves the user's address into a variable
 
-    
-
-
-    
     print(f'Your profile: ID: {"0" + str(ID)}, name: {NAME.upper().strip()}, DOB: {DOB[0:2] + "/" + DOB[3:5] + "/" + DOB[6:10]}, Address: {ADDRESS.strip().lower()}')
+    # Code above prints all the inputed variables into a profile.
     
 profileGenerator()
 
@@ -57,10 +59,12 @@ profileGenerator()
 # Question 3 #
 ##############
 
+# Below is a function that asks the user for a number and returns how many digits the number has
 
 def digiter():
-    digits = input('Input a number: ')
-    print(f'{digits} has {len(digits)} digits')
+
+    digits = input('Input a number: ') # asks the user for a number and saves it into a variable
+    print(f'{digits} has {len(digits)} digits') # Returns how many digits are in said number
 
 digiter()
 
@@ -68,9 +72,13 @@ digiter()
 # Question 4 #
 ##############
 
+# Below is a function that asks the user for a numeric grade and returns a letter grade
+
 def gradeConverter():
-    numeric_grade = int(input("What is your numeric grade? "))
-    letter_grade = ""
+    numeric_grade = int(input("What is your numeric grade? ")) # Saves numeric grade into a variable
+    letter_grade = "" # initial letter grade variable
+
+    # Below code converts the numeric grade into a letter grade then prints it
     if numeric_grade >= 90:
         if numeric_grade >= 97:
             letter_grade = "A+"
@@ -113,11 +121,17 @@ gradeConverter()
 # Question 5 #
 ##############
 
+# Below is a function that prints an astrix pattern based on the number a user inputs.
+
 def patterner():
-    number = int(input("Enter a number to see something cool: "))
-    astrix = "*"
+    number = int(input("Enter a number to see something cool: ")) # Saves the number into a variable
+    astrix = "*" # The initial "*" variable that is going to be used to print the pattern
+
+    # Below code loops through using the number and prints "*" accordingly
     for i in range(1, number + 1):
         print(astrix * i)
+
+    # Does the same thing as the above loop but iterates in reverse
     for i in range(number - 1, -1, -1):
         print(astrix * i)
 
@@ -127,12 +141,16 @@ patterner()
 # Question 6 #
 ##############
 
+# Below is a function that takes a range of two numbers and returns the even numbers between them.
+
 def evenFinder():
-    number1 = int(input('Enter the first number in your range: '))
-    number2 = int(input('Enter the second number in your range: '))
-    while(number2 < number1):
+    number1 = int(input('Enter the first number in your range: ')) # Saves the first number into a variable
+    number2 = int(input('Enter the second number in your range: ')) # Saves the second number into a variable
+
+    while(number2 < number1): #A loop that asks the user to re-input the second number if it is smaller than the first one
         number2 = int(input('Your second number cannot be smaller than your first number, input a new second number: '))
     
+    # A loop that prints any number that returns 0 when divided by two AKA an even number.
     for i in range(number1, number2 + 1):
         if(i % 2 == 0):
             print(i)
